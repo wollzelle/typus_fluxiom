6a8ce6b429e581574feedeaec36734dc565d7a9f
Thomas König <t.koenig@wollzelle.com>
2010-12-20 12:23:32 +0100

    preliminary work to use static assets for gems on heroku
    not functional yet, just as a reference

--- lib/flux_assets/engine.rb
+++ lib/flux_assets/engine.rb
@@ -3,12 +3,15 @@ require "rails"
 
 module FluxAssets
  class Engine < Rails::Engine
+
+   initializer "static assets" do |app|
+         app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+   end     
    
    def initialize
      FluxAssets::Configuration.config!
      # Cms::FormBuilder.send(:include, Cms::Fluxiom::FormBuilder)
      ActionView::Base.send(:include, FluxAssets::ApplicationHelper)
    end
-     
   end
 end
