8be6dc1abb5736b684e234bcfac8c97cfc7cc03b
meleyal <william.meleyal@gmail.com>
2011-01-31 11:20:38 +0100

    display filter_tag first, disabe caching in dev environment

--- app/controllers/fluxiom_controller.rb
+++ app/controllers/fluxiom_controller.rb
@@ -22,9 +22,19 @@ class FluxiomController < ApplicationController
     # select tags
     @tags ||= begin
       tags = []
-      @assets.each { |a| tags = tags | a.tags }
+      @assets.each do |a| 
+        #get the asset tags – if there is a filter tag defined in the config, remove it from the main tags and assign it to @filter_tag
+        asset_tags = a.tags.delete_if { |t|
+          is_filter_tag = t.tag == FluxiomAsset::FILTER_TAG
+          @filter_tag = t if is_filter_tag
+          is_filter_tag
+        } if FluxiomAsset::FILTER_TAG
+        tags = tags | asset_tags
+      end
       tags
     end    
+    
+    
   end
 
   def list
@@ -43,7 +53,7 @@ class FluxiomController < ApplicationController
   end
 
   def set_cache
-    response.headers['Cache-Control'] = 'public, max-age=300'
+    response.headers['Cache-Control'] = 'public, max-age=300' unless Rails.env.development?
   end
 
 end
