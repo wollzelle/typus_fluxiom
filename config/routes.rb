a0131b2a14cb6eafff7dccd6ce69591f304c3395
meleyal <william.meleyal@gmail.com>
2011-05-26 15:51:29 +0200

    fixes Rails router DSL deprecation warning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-Rails.application.routes.draw do |map|
-#  namespace :admin do
-    resources :fluxiom, :only => [:index] do
-      get 'list', :on => :collection
-    end
-#  end
+Rails.application.routes.draw do
+  resources :fluxiom, :only => [:index] do
+    get :list, :on => :collection
+  end
 end
