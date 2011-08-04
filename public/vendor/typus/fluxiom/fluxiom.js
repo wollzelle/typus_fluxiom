4aba8b3d132765534e12f6fc7694bd8ef0ed506b
meleyal <william.meleyal@gmail.com>
2011-08-04 12:45:07 +0200

    fix tag loading

--- public/vendor/typus/fluxiom/fluxiom.js
+++ public/vendor/typus/fluxiom/fluxiom.js
@@ -17,13 +17,15 @@ var Flux = {
   initialize: function(){
     _.templateSettings = { interpolate : /\{\{(.+?)\}\}/g };
     this.assets = new Flux.Models.Assets();
-    this.assets.bind('loaded', _.bind(this.getTags, this));
+    this._getTags = _.bind(this.getTags, this);
+    this.assets.bind('loaded', this._getTags);
     this.selectedAssets = new Flux.Models.SelectedAssets();
     this.chooser = new Flux.Views.Chooser();
   },
 
   // this is needed for authorization in Firefox
   getTags: function(){
+    this.assets.unbind('loaded', this._getTags);
     this.tags = new Flux.Models.Tags();
   }
 
