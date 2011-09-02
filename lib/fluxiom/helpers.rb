74a63a95ff23b14a85bcc197eb7167aa756538eb
meleyal <william.meleyal@gmail.com>
2011-09-02 11:46:31 +0200

    flux_preview_for

--- lib/fluxiom/helpers.rb
+++ lib/fluxiom/helpers.rb
@@ -1,9 +1,15 @@
 module Fluxiom
   module ApplicationHelper
-    def flux_image_for(path, format)
+
+    def flux_capacitor_for(path, format)
       return "#" if path.nil? || format.nil?
-      return path.gsub(/_.*/, "_#{format}")
+      path = path.match(/(.*\/([^_|.]*))/)[0] # returns the path without format
+      format.gsub!(/^([^._])(.*)/, '_\1\2') # clean up the format
+      return path+format
     end
+
+    alias :flux_image_for :flux_capacitor_for
+    alias :flux_preview_for :flux_capacitor_for
   end
 end
 
