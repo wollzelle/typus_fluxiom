ff345c53a4ebcec2bd42a096cb1273ac901b6385
meleyal <william.meleyal@gmail.com>
2011-10-19 16:37:58 +0200

    Move fluxiom to admin namespace (/admin/fluxiom)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :fluxiom, :only => [:index] do
-    get :assets, :on => :collection
-    get :tags, :on => :collection
+  namespace :admin do
+    resources :fluxiom, :only => [:index] do
+      get :assets, :on => :collection
+      get :tags, :on => :collection
+    end
   end
 end
